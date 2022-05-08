7fbf69fb0c45cf6519d340d98f9c557986332bef
米泽榕 <943722704@>
2022-05-08 22:07:56 +0800

    init

--- src/main/java/com/mzr/entity/Category.java
+++ src/main/java/com/mzr/entity/Category.java
@@ -1,5 +1,11 @@
 package com.mzr.entity;
 
+import lombok.Data;
+import lombok.ToString;
+import org.apache.ibatis.annotations.Select;
+
+@ToString
+@Data
 public class Category {
     private String Id;
     private String Code;
